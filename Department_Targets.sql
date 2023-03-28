c089ddabcb4a33fcc1d4477dffa9d310004dc6bb
vmose <vicmose.vm@gmail.com>
2023-03-28 23:51:24 +0300

    Update Department_Targets.sql

--- Department_Targets.sql
+++ Department_Targets.sql
@@ -1,15 +1,25 @@
 SELECT 
-    emp.first_name, 
-    emp.last_name, 
-    emp.sales, 
-    dep.department_name, 
-    DENSE_RANK() OVER (
-        PARTITION BY dep.department_id 
-        ORDER BY emp.sales DESC
-    ) AS sales_rank
+  CONCAT_WS(' ', first_name, last_name) AS full_name,
+  SUM(CASE 
+    WHEN order_date BETWEEN '2022-01-01' AND '2022-03-31' THEN total_price 
+    ELSE 0 
+  END) AS q1_sales,
+  SUM(CASE 
+    WHEN order_date BETWEEN '2022-04-01' AND '2022-06-30' THEN total_price 
+    ELSE 0 
+  END) AS q2_sales,
+  SUM(CASE 
+    WHEN order_date BETWEEN '2022-07-01' AND '2022-09-30' THEN total_price 
+    ELSE 0 
+  END) AS q3_sales,
+  SUM(CASE 
+    WHEN order_date BETWEEN '2022-10-01' AND '2022-12-31' THEN total_price 
+    ELSE 0 
+  END) AS q4_sales
 FROM 
-    employees emp
-    JOIN departments dep ON emp.department_id = dep.department_id
-ORDER BY 
-    dep.department_name, 
-    sales_rank;
+  orders 
+  JOIN customers ON orders.customer_id = customers.customer_id
+GROUP BY 
+  full_name
+HAVING 
+  q1_sales + q2_sales + q3_sales + q4_sales > 10000
