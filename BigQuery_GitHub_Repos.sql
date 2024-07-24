389a16e9350cd557e23af00f56e2889ef09cd085
vmose <80821950+vmose@users.noreply.github.com>
2024-07-24 14:04:55 +0300

    Update BigQuery_GitHub_Repos.sql

--- BigQuery_GitHub_Repos.sql
+++ BigQuery_GitHub_Repos.sql
@@ -1,34 +1,34 @@
 WITH RECURSIVE CommitHistory AS (
-    SELECT 
-        repo_name,
-        sha AS commit_sha,
-        NULL AS parent_commit_sha,
-        author.name AS author_name,
-        committer.name AS committer_name,
-        committed_date
-    FROM 
-        `bigquery-public-data.github_repos.commits`
-    WHERE 
-        parent IS NULL
-    LIMIT 1
+  SELECT
+    repo_name,
+    commit,
+    parent[OFFSET(0)] AS parent_commit,
+    author.name AS author_name,
+    committer.name AS committer_name,
+    committer.date AS committer_date
+  FROM
+    `bigquery-public-data.github_repos.sample_commits`
+  WHERE
+    ARRAY_LENGTH(parent) = 0
 
-    UNION ALL
+  UNION ALL
 
-    SELECT 
-        c.repo_name,
-        c.sha AS commit_sha,
-        c.parent[OFFSET(0)] AS parent_commit_sha,
-        c.author.name AS author_name,
-        c.committer.name AS committer_name,
-        c.committed_date
-    FROM 
-        `bigquery-public-data.github_repos.commits` c
-    JOIN 
-        CommitHistory ch
-    ON 
-        c.sha = ch.parent_commit_sha
+  SELECT
+    c.repo_name,
+    c.commit,
+    c.parent[OFFSET(0)] AS parent_commit,
+    c.author.name AS author_name,
+    c.committer.name AS committer_name,
+    c.committer.date AS committer_date
+  FROM
+    `bigquery-public-data.github_repos.sample_commits` c
+  JOIN
+    CommitHistory ch
+  ON
+    c.commit = ch.parent_commit
 )
-SELECT 
-    * 
-FROM 
-    CommitHistory;
+SELECT
+  *
+FROM
+  CommitHistory;
+
